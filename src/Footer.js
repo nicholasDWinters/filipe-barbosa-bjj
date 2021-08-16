454522059d1caf1f0dabccb9d412f016986ffee5
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-08-16 11:32:43 -0500

    updated footer sizing

--- src/Footer.js
+++ src/Footer.js
@@ -6,10 +6,10 @@ import facebook from './images/facebook_icon.png';
 const Footer = () => {
     return (
         <div className='Footer mt-5 container-fluid'>
-            <h3 className='pt-4 ms-3'>FILIPE BARBOSA JIU JITSU</h3>
-            <h5 className='mt-4 ms-3'>912 WIS-136 E, STE.104</h5>
-            <h5 className='ms-3'>BARABOO, WI 53913</h5>
-            <h5 className='ms-3'>608.477.0021</h5>
+            <h4 className='pt-3 ms-3'>FILIPE BARBOSA JIU JITSU</h4>
+            <h6 className='mt-2 ms-3'>912 WIS-136 E, STE.104</h6>
+            <h6 className='ms-3'>BARABOO, WI 53913</h6>
+            <h6 className='ms-3'>608.477.0021</h6>
             <div className='mt-3 ms-3'>
                 <a className='me-4' href='https://www.facebook.com/Filipe-Barbosa-Jiu-Jitsu-AcademyCRBJJ-190576607652167' target='_blank' rel='noreferrer'><img src={facebook} alt="facebook" width="30" height="30" className="d-inline-block"></img></a>
                 <a className='me-4' href='https://www.instagram.com/filipebarbosabjj/?hl=en' target='_blank' rel='noreferrer'><img src={instagram} alt="instagram" width="30" height="30" className="d-inline-block"></img></a>
