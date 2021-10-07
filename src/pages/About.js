252aca460cb653c9f08678b7e9389914be6ca43e
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-10-06 22:24:08 -0500

    updated about page, got rid of overflow on filipe paragraph

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,7 +15,7 @@ const About = () => {
                 <div className='col-lg-5 paragraph-col mb-3'>
                     <h1 className=''>FILIPE BARBOSA</h1>
                     <h4 className="text-muted">Owner, Head Instructor, 2nd Degree Black Belt</h4>
-                    <div className='overflow-auto Overflow' style={{ height: '350px' }}>
+                    <div className='overflow-auto Overflow About-paragraph'>
                         <p className='paragraph about'>I’ve been involved with martial arts since my childhood. I started with Judo in Recife, Brazil, where I was born, and loved it! I learned this beautiful art of Judo with Professor Nagai, a legendary instructor and great inspirational leader! After that, I tried many different forms of martial arts… and found Karate: Kyokushin Oyama style, the most full contact form of Karate! I continued trying to find the perfect martial art, realizing there were many different styles and cultures to choose from. One day I watched the best Kyokushin brown belt from my gym get dominated and choked out by a man called “Coelho” (Rabbit). This man was a fighter; using a martial art with a very different approach that was unknown – to me at least!! JIU-JITSU!!! Intrigued, I finally began my training in what most consider the most effective and exciting form of self defense; known today as Brazilian Jiu-Jitsu! I initiated this training in Manaus, Brazil (The capitol of the Amazonas state) training with the Monteiro Brothers, under Royler Gracie. It immediately became my love and my passion! At that time I attended many international tournaments and seminars. This brought me to America where I started training Jiu-Jitsu with Luiz Claudio and Thiago Veiga. I’ve accomplished many goals as purple belt, brown belt, black belt, instructor, father and friend; with hopes of giving back to YOU by helping you achieve your goals through hard work and dedication.</p></div></div>
                 <div className='col-lg-1'></div>
                 <div className='col-lg-5 mt-3 mt-lg-0'>
@@ -28,7 +28,7 @@ const About = () => {
                     <h1 className=''>CRISTIANO RIBEIRO</h1>
                     <h4 className="text-muted">Owner & Instructor of Gracie Barra - Madison</h4>
                     <h4 className="text-muted">4th Degree Black Belt</h4>
-                    <p className='paragraph about'>Professor Riberio is a Brazilian Jiu-Jitsu instructor and Professional competitor from Manaus, Brazil. Royler Gracie presented him with his Black Belt at the age of 17. He continues to compete around the country and instructs regularly scheduled classes and privately.</p></div>
+                    <p className='paragraph about'>Professor Riberio is a Brazilian Jiu-Jitsu instructor and Professional competitor from Manaus, Brazil. He is the Owner & Head Instructor of Gracie Barra - Madison, and represents Gracie Barra Founder, Master Carlos Gracie Jr. He continues to compete around the country and instructs regularly scheduled classes and privately.</p></div>
                 <div className='col-lg-1'></div>
 
                 <div className='col-lg-5 mt-3 mt-lg-0'>
