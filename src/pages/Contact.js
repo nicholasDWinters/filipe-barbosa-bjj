486061eeed5db1b008721a16c0b1948ccf69c491
Nicholas Winters <nicholas.d.winters@gmail.com>
2023-12-27 15:17:44 -0600

    quick update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,6 @@ import React from 'react';
 import instagram from '../images/instagram_icon.png';
 import facebook from '../images/facebook_icon.png';
 import messenger from '../images/messenger_icon.png';
-import contact from '../images/contact.jpg';
 // import API_KEY from '../api';
 import Form from '../Form';
 import './Contact.css';
@@ -14,7 +13,6 @@ const Contact = () => {
 
     // const KEY = process.env.API_KEY;
     let url = `https://www.google.com/maps/embed/v1/place?q=place_id:ChIJpe9wKhxCB4gRQBEHVjWw-uI&key=${KEY}`;
-    const contactPic = `${contact}`;
 
     return (
         <div className='container'>
@@ -29,10 +27,6 @@ const Contact = () => {
                     <h4 className="text-muted mb-3">608.477.0021</h4>
                     <a href='https://www.facebook.com/Filipe-Barbosa-Jiu-Jitsu-AcademyCRBJJ-190576607652167' target='_blank' rel='noreferrer'><img src={facebook} alt="facebook" width="45" height="45" className="d-inline-block align-text-top me-4"></img></a>
                     <a href='https://www.instagram.com/filipebarbosabjj/?hl=en' target='_blank' rel='noreferrer'><img src={instagram} alt="instagram" width="45" height="45" className="d-inline-block align-text-top me-4"></img></a>
-                    {/* <div className='d-block mt-3 mb-3'>
-                        <div className="fb-messengermessageus" messenger_app_id='301920271682228' page_id='190576607652167' color="blue">
-                        </div>
-                    </div> */}
                     <a href='https://www.messenger.com/t/190576607652167/?messaging_source=source%3Apages%3Amessage_shortlink' className='btn btn-outline-primary mt-3' target='_blank' rel='noreferrer'><img src={messenger} alt="messenger" width="45" height="45" className="d-inline-block me-1"></img>Chat with us on Messenger</a>
 
                 </div>
@@ -41,13 +35,7 @@ const Contact = () => {
                     <Form formType={'generalInfo'}/>
                 </div>
                 <div className="row align-items-center">
-
-
-                <div className='col-lg-5'>
-                <img className='img-fluid' src={contact} alt='team on competition podium'></img>
-                </div>
-                <div className='col-lg-1'></div>
-                <div className='col-lg-5'>
+                <div className='col-lg-11'>
                     <iframe title='map around academy'
                         width="100%"
                         height="400px"
